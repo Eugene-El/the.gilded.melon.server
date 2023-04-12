1deb6c278733f40786395ad50def024a012fcf5e
Eugene L <lomashonok@inbox.lv>
2023-04-13 01:23:27 +0300

    IP update

--- src/app/helpers/constants.ts
+++ src/app/helpers/constants.ts
@@ -1,4 +1,6 @@
 
-export const SERVER_IP: string = "80.82.222.184";
+export const SERVER_IP: string = "194.163.188.162";
 export const SERVER_PORT: string = "25566";
+export const MAP_PORT: string = "8100";
 export const SERVER_ADDRESS: string = `${SERVER_IP}:${SERVER_PORT}`;
+export const MAP_ADDRESS: string = `http://${SERVER_IP}:${MAP_PORT}/`;
--- src/app/routers/app-routing.module.ts
+++ src/app/routers/app-routing.module.ts
@@ -5,13 +5,14 @@ import { GentlemanComponent } from '../pages/gentleman/gentleman.component';
 import { HomeComponent } from '../pages/home/home.component';
 import { NotFoundComponent } from '../pages/not-found/not-found.component';
 import { RulesComponent } from '../pages/rules/rules.component';
+import { MAP_ADDRESS } from '../helpers/constants';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'rules', component: RulesComponent },
   { path: 'not-found', component: NotFoundComponent },
   { path: 'gentleman', component: GentlemanComponent },
-  { path: 'map', loadChildren: () => new Promise(() => openExternalUrl("http://80.82.222.184:8100/")) },
+  { path: 'map', loadChildren: () => new Promise(() => openExternalUrl(MAP_ADDRESS)) },
   { path: 'discord', loadChildren: () => new Promise(() => openExternalUrl("https://discord.gg/sGhxfbxryc")) },
   { path: 'plugins', loadChildren: () => import('../modules/plugins.module').then(m => m.PluginsModule) },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
